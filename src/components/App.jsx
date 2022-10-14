7be00e492300f366610f07c02a28644eb8e1e577
Stanislaw Grygoriew <stanislaw.grygoriew@gmail.com>
2022-10-14 12:38:49 +0200

    refactored task to hooks ;

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,64 +1,80 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import { FeedbackOptions } from './FeedbackOptions/';
 import { Section } from './Section/';
 import { Statistics } from './Statistics/';
 import { Notification } from './Notification/';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [positivePercentage, setPositivePercentage] = useState(0);
 
-  addReview = e => {
+  const reviewOptions = ['good', 'neutral', 'bad'];
+
+  const addReview = e => {
     const key = e.currentTarget.name;
-    this.setState(prevState => ({ [key]: (prevState[key] += 1) }));
+
+    switch (key) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        return;
+
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        return;
+
+      case 'bad':
+        setBad(prevState => prevState + 1);
+        return;
+
+      default:
+        console.log('There is no such option');
+        return;
+    }
   };
 
-  countTotalReviews() {
-    return Object.values(this.state).reduce((a, b) => a + b);
-  }
+  useEffect(() => {
+    setTotal(good + neutral + bad);
+  }, [good, neutral, bad]);
 
-  countPositiveReviewsPercentage() {
-    return parseInt((this.state.good / this.countTotalReviews()) * 100);
-  }
+  useEffect(() => {
+    setPositivePercentage(parseInt((good / total) * 100));
+  }, [good, total]);
 
-  render() {
-    const { good, bad, neutral } = this.state;
-    return (
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 40,
-          color: '#010101',
-        }}
-      >
-        <Section title="Please leave your feedback">
-          <FeedbackOptions
-            options={Object.keys(this.state)}
-            onLeaveFeedback={this.addReview}
-          ></FeedbackOptions>
-        </Section>
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      <Section title="Please leave your feedback">
+        <FeedbackOptions
+          options={reviewOptions}
+          onLeaveFeedback={addReview}
+        ></FeedbackOptions>
+      </Section>
 
-        <Section title="Statistics">
-          {this.countTotalReviews() > 0 ? (
-            <Statistics
-              goodReviews={good}
-              neutralReviews={neutral}
-              badReviews={bad}
-              totalReviews={this.countTotalReviews()}
-              positiveReviewsPercentage={this.countPositiveReviewsPercentage()}
-            ></Statistics>
-          ) : (
-            <Notification message="There is no feedback yet"></Notification>
-          )}
-        </Section>
-      </div>
-    );
-  }
-}
+      <Section title="Statistics">
+        {total > 0 ? (
+          <Statistics
+            goodReviews={good}
+            neutralReviews={neutral}
+            badReviews={bad}
+            totalReviews={total}
+            positiveReviewsPercentage={positivePercentage}
+          ></Statistics>
+        ) : (
+          <Notification message="There is no feedback yet"></Notification>
+        )}
+      </Section>
+    </div>
+  );
+};
